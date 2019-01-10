3991c21f19cde7576f826d56abded3644f6b9742
Seven0904 <691440508@qq.com>
2019-01-10 22:25:18 +0800

    增加去登录页面的功能

--- src/system/Register.vue
+++ src/system/Register.vue
@@ -17,96 +17,97 @@
         </el-form-item>
         <el-form-item style="width: 100%">
             <el-button type="primary" @click="submitForm('registerForm')" style="width: 100%">注册</el-button>
-            <!--<el-button @click="resetForm('loginForm')">重置</el-button>-->
         </el-form-item>
+        <span @click="$router.push('/login')">已有账号，去登录</span>
     </el-form>
 </template>
 
 <script>
     import * as api from "../api/api"
     import * as RestCode from "../common/RestCode"
+
     export default {
         name: "Register",
-        data(){
-        var validatePassword = (rule, value, callback) => {
-            // if (value === '') {
-            //     this.$message.error('请再次输入密码');
-            // } else if (value !== this.registerRules.password) {
-            //     this.$message.error('两次输入密码不一致');
-            // }
-            // var vm = this
-            // console.log(value,this.registerForm.password)
-            if (value === '' || typeof(value) == "undefined") {
-                callback(new Error('请再次输入密码'));
-            } else if (value !== this.registerForm.password) {
-                callback(new Error('两次输入密码不一致!'));
-            } else {
-                callback();
-            }
-
-        };
-
-    return {
-        registerForm: {
-            email: '',
-            account: '',
-            password: '',
-            checkPass: '',
-        },
-        registerRules: {
-            email: [
-                {required: true, message: '请输入邮箱', trigger: 'blur'},
-                {type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change']}
-            ],
-            account: [
-                {required: true, message: '请输入账号', trigger: 'blur'}
-            ],
-            password: [
-                {required: true, message: '请输入密码', trigger: 'blur'}
-            ],
-            checkPassword: [
-                {required: true, validator: validatePassword, trigger: 'blur'}
-            ],
-        },
-    };
-
-    },
-    methods: {
-        submitForm(formName) {
-            let vm = this;
-            this.$refs[formName].validate((valid) => {
-                if (valid) {
-                    api.requestRegister({
-                        'email': vm.registerForm.email,
-                        'name': vm.registerForm.account,
-                        'password': vm.registerForm.password
-                    }).then((res) => {
-                        let code = res.code;
-                        let msg = res.msg;
-                        if (code === RestCode.SUCCESS) {
-
-
-                            this.$message.success('注册成功');
-                            vm.$router.push("/login");
-                        } else if (code === RestCode.RECORD_ALREADY_EXISTS) {
-                            this.$message.warning('用户已存在');
-                        }
-                        else {
-                            this.$message.error('注册失败。' + msg);
-                        }
-
-
-                    });
+        data() {
+            var validatePassword = (rule, value, callback) => {
+                // if (value === '') {
+                //     this.$message.error('请再次输入密码');
+                // } else if (value !== this.registerRules.password) {
+                //     this.$message.error('两次输入密码不一致');
+                // }
+                // var vm = this
+                // console.log(value,this.registerForm.password)
+                if (value === '' || typeof(value) == "undefined") {
+                    callback(new Error('请再次输入密码'));
+                } else if (value !== this.registerForm.password) {
+                    callback(new Error('两次输入密码不一致!'));
                 } else {
-                    console.log('error submit!!');
-                    return false;
+                    callback();
                 }
-            });
+
+            };
+
+            return {
+                registerForm: {
+                    email: '',
+                    account: '',
+                    password: '',
+                    checkPass: '',
+                },
+                registerRules: {
+                    email: [
+                        {required: true, message: '请输入邮箱', trigger: 'blur'},
+                        {type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change']}
+                    ],
+                    account: [
+                        {required: true, message: '请输入账号', trigger: 'blur'}
+                    ],
+                    password: [
+                        {required: true, message: '请输入密码', trigger: 'blur'}
+                    ],
+                    checkPassword: [
+                        {required: true, validator: validatePassword, trigger: 'blur'}
+                    ],
+                },
+            };
+
         },
-        // resetForm(formName) {
-        //     this.$refs[formName].resetFields();
-        // }
-    }
+        methods: {
+            submitForm(formName) {
+                let vm = this;
+                this.$refs[formName].validate((valid) => {
+                    if (valid) {
+                        api.requestRegister({
+                            'email': vm.registerForm.email,
+                            'name': vm.registerForm.account,
+                            'password': vm.registerForm.password
+                        }).then((res) => {
+                            let code = res.code;
+                            let msg = res.msg;
+                            if (code === RestCode.SUCCESS) {
+
+
+                                this.$message.success('注册成功');
+                                vm.$router.push("/login");
+                            } else if (code === RestCode.RECORD_ALREADY_EXISTS) {
+                                this.$message.warning('用户已存在');
+                            }
+                            else {
+                                this.$message.error('注册失败。' + msg);
+                            }
+
+
+                        });
+                    } else {
+                        console.log('error submit!!');
+                        return false;
+                    }
+                });
+            },
+            // resetForm(formName) {
+            //     this.$refs[formName].resetFields();
+            // }
+        }
     }
 </script>
 
@@ -133,4 +134,12 @@
         /*margin-top: 10px;*/
     }
 
+    span {
+        font-size: 14px;
+        position: relative;
+        left: 186px;
+    }
+    span:hover{
+        cursor:pointer
+    }
 </style>
